2043b6a36081804f13f3cb7734fd197f377b53f8
John <95032031+DMTmaster2000@users.noreply.github.com>
2022-11-16 15:30:33 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4398,6 +4398,32 @@
             goToHash ? gotoBlock(goToHash, true, 500, 20) : null;
         }
     }
+    function headerScroll() {
+        addWindowScrollEvent = true;
+        const header = document.querySelector(".header");
+        const headerShow = header.hasAttribute("data-scroll-show");
+        const headerShowTimer = header.dataset.scrollShow ? header.dataset.scrollShow : 500;
+        const startPoint = header.dataset.scroll ? header.dataset.scroll : 1;
+        let scrollDirection = 0;
+        let timer;
+        document.addEventListener("windowScroll", (function(e) {
+            const scrollTop = window.scrollY;
+            clearTimeout(timer);
+            if (scrollTop >= startPoint) {
+                !header.classList.contains("_header-scroll") ? header.classList.add("_header-scroll") : null;
+                if (headerShow) {
+                    if (scrollTop > scrollDirection) header.classList.contains("_header-show") ? header.classList.remove("_header-show") : null; else !header.classList.contains("_header-show") ? header.classList.add("_header-show") : null;
+                    timer = setTimeout((() => {
+                        !header.classList.contains("_header-show") ? header.classList.add("_header-show") : null;
+                    }), headerShowTimer);
+                }
+            } else {
+                header.classList.contains("_header-scroll") ? header.classList.remove("_header-scroll") : null;
+                if (headerShow) header.classList.contains("_header-show") ? header.classList.remove("_header-show") : null;
+            }
+            scrollDirection = scrollTop <= 0 ? 0 : scrollTop;
+        }));
+    }
     setTimeout((() => {
         if (addWindowScrollEvent) {
             let windowScroll = new Event("windowScroll");
@@ -4407,8 +4433,8 @@
         }
     }), 0);
     function menuCloseBag() {
-        document.querySelector(".menu__body");
         if (document.documentElement.clientWidth <= 671) {
+            document.querySelector(".menu__body");
             const items = document.querySelectorAll(".menu__item");
             items.forEach((item => document.addEventListener("click", (function(e) {
                 if (e.target.closest(".menu__item")) {
@@ -4429,4 +4455,5 @@
     });
     formSubmit();
     pageNavigation();
+    headerScroll();
 })();
